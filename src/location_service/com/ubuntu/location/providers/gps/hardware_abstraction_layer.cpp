9a1ee66d681a37c68991dbf26fbf0acce9081a01
thomas-voss <thomas.voss@canonical.com>
2014-06-09 10:58:06 +0200

    Make sure to report APN to gps chipset.

--- src/location_service/com/ubuntu/location/providers/gps/hardware_abstraction_layer.cpp
+++ src/location_service/com/ubuntu/location/providers/gps/hardware_abstraction_layer.cpp
@@ -310,6 +310,7 @@ struct HardwareAbstractionLayer : public gps::HardwareAbstractionLayer
         {
         case U_HARDWARE_GPS_REQUEST_AGPS_DATA_CONN:
             VLOG(1) << "U_HARDWARE_GPS_REQUEST_AGPS_DATA_CONN";
+            thiz->impl.supl_assistant.notify_data_connection_open_via_apn("internet");
             thiz->inject_reference_position(location::Position
             {
                location::wgs84::Latitude{51.444670 * location::units::Degrees},
