c5bbe4c322a8de634b8eee2a14da80ebe8c9eec6
thomas-voss <thomas.voss@canonical.com>
2014-06-10 12:08:48 +0200

    Print last seen timestamp.

--- src/location_service/com/ubuntu/location/connectivity/wireless_network.cpp
+++ src/location_service/com/ubuntu/location/connectivity/wireless_network.cpp
@@ -36,9 +36,12 @@ std::ostream& location::connectivity::operator<<(std::ostream& out, location::co
 
 std::ostream& location::connectivity::operator<<(std::ostream& out, const location::connectivity::WirelessNetwork& wifi)
 {
+    auto ts = std::chrono::system_clock::to_time_t(wifi.last_seen().get());
+
     return out << "("
                << "bssid: " << wifi.bssid().get() << ", "
                << "ssid: " << wifi.ssid().get() << ", "
+               << "last seen: " << std::ctime(&ts) << ", "
                << "mode: " << wifi.mode().get() << ", "
                << "frequency: " << wifi.frequency().get() << ", "
                << "strength: " << wifi.signal_strength().get()
