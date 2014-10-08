92a28c3f3c2831fd240de0f7f523a90ed4aba73d
thomas-voss <thomas.voss@canonical.com>
2014-10-08 16:17:54 +0200

    If either network manager or ofono are not present, postpone initialization until the name is registered on the bus. Add first cut of automated acceptance tests for connectivity API.

--- src/location_service/com/ubuntu/location/connectivity/ofono_nm_connectivity_manager.cpp
+++ src/location_service/com/ubuntu/location/connectivity/ofono_nm_connectivity_manager.cpp
@@ -18,6 +18,8 @@
 
 #include "ofono_nm_connectivity_manager.h"
 
+#include <core/dbus/dbus.h>
+
 namespace connectivity = com::ubuntu::location::connectivity;
 namespace dbus = core::dbus;
 namespace xdg = org::freedesktop;
@@ -40,32 +42,36 @@ connectivity::Characteristics all_characteristics()
 }
 }
 
-const core::Property<connectivity::State>& impl::OfonoNmConnectivityManager::state() const
+connectivity::OfonoNmConnectivityManager::OfonoNmConnectivityManager(const core::dbus::Bus::Ptr& bus) : d(bus)
+{
+}
+
+const core::Property<connectivity::State>& connectivity::OfonoNmConnectivityManager::state() const
 {
     return d.state;
 }
 
-const core::Property<bool>& impl::OfonoNmConnectivityManager::is_wifi_enabled() const
+const core::Property<bool>& connectivity::OfonoNmConnectivityManager::is_wifi_enabled() const
 {
-    return *d.network_manager->properties.is_wifi_enabled;
+    return d.cached.is_wifi_enabled;
 }
 
-const core::Property<bool>& impl::OfonoNmConnectivityManager::is_wwan_enabled() const
+const core::Property<bool>& connectivity::OfonoNmConnectivityManager::is_wwan_enabled() const
 {
-    return *d.network_manager->properties.is_wwan_enabled;
+    return d.cached.is_wwan_enabled;
 }
 
-const core::Property<bool>& impl::OfonoNmConnectivityManager::is_wifi_hardware_enabled() const
+const core::Property<bool>& connectivity::OfonoNmConnectivityManager::is_wifi_hardware_enabled() const
 {
-    return *d.network_manager->properties.is_wifi_hardware_enabled;
+    return d.cached.is_wifi_hardware_enabled;
 }
 
-const core::Property<bool>& impl::OfonoNmConnectivityManager::is_wwan_hardware_enabled() const
+const core::Property<bool>& connectivity::OfonoNmConnectivityManager::is_wwan_hardware_enabled() const
 {
-    return *d.network_manager->properties.is_wwan_hardware_enabled;
+    return d.cached.is_wwan_hardware_enabled;
 }
 
-void impl::OfonoNmConnectivityManager::request_scan_for_wireless_networks()
+void connectivity::OfonoNmConnectivityManager::request_scan_for_wireless_networks()
 {
     std::lock_guard<std::mutex> lg(d.cached.guard);
 
@@ -73,39 +79,39 @@ void impl::OfonoNmConnectivityManager::request_scan_for_wireless_networks()
         pair.second.request_scan();
 }
 
-const core::Signal<>& impl::OfonoNmConnectivityManager::wireless_network_scan_finished() const
+const core::Signal<>& connectivity::OfonoNmConnectivityManager::wireless_network_scan_finished() const
 {
     return d.signals.wireless_network_scan_finished;
 }
 
-const core::Signal<connectivity::WirelessNetwork::Ptr>& impl::OfonoNmConnectivityManager::wireless_network_added() const
+const core::Signal<connectivity::WirelessNetwork::Ptr>& connectivity::OfonoNmConnectivityManager::wireless_network_added() const
 {
     return d.signals.wireless_network_added;
 }
 
-const core::Signal<connectivity::WirelessNetwork::Ptr>& impl::OfonoNmConnectivityManager::wireless_network_removed() const
+const core::Signal<connectivity::WirelessNetwork::Ptr>& connectivity::OfonoNmConnectivityManager::wireless_network_removed() const
 {
     return d.signals.wireless_network_removed;
 }
 
-void impl::OfonoNmConnectivityManager::enumerate_visible_wireless_networks(const std::function<void(const connectivity::WirelessNetwork::Ptr&)>& f) const
+void connectivity::OfonoNmConnectivityManager::enumerate_visible_wireless_networks(const std::function<void(const connectivity::WirelessNetwork::Ptr&)>& f) const
 {
     std::lock_guard<std::mutex> lg(d.cached.guard);
     for (const auto& wifi : d.cached.wifis)
         f(wifi.second);
 }
 
-const core::Signal<connectivity::RadioCell::Ptr>& impl::OfonoNmConnectivityManager::connected_cell_added() const
+const core::Signal<connectivity::RadioCell::Ptr>& connectivity::OfonoNmConnectivityManager::connected_cell_added() const
 {
     return d.signals.connected_cell_added;
 }
 
-const core::Signal<connectivity::RadioCell::Ptr>& impl::OfonoNmConnectivityManager::connected_cell_removed() const
+const core::Signal<connectivity::RadioCell::Ptr>& connectivity::OfonoNmConnectivityManager::connected_cell_removed() const
 {
     return d.signals.connected_cell_removed;
 }
 
-void impl::OfonoNmConnectivityManager::enumerate_connected_radio_cells(const std::function<void(const connectivity::RadioCell::Ptr&)>& f) const
+void connectivity::OfonoNmConnectivityManager::enumerate_connected_radio_cells(const std::function<void(const connectivity::RadioCell::Ptr&)>& f) const
 {
     std::lock_guard<std::mutex> lg(d.cached.guard);
     // We only report currently valid cells.
@@ -114,53 +120,67 @@ void impl::OfonoNmConnectivityManager::enumerate_connected_radio_cells(const std
             f(cell.second);
 }
 
-const core::Property<connectivity::Characteristics>& impl::OfonoNmConnectivityManager::active_connection_characteristics() const
+const core::Property<connectivity::Characteristics>& connectivity::OfonoNmConnectivityManager::active_connection_characteristics() const
 {
     return d.active_connection_characteristics;
 }
 
-impl::OfonoNmConnectivityManager::Private::Private()
+connectivity::OfonoNmConnectivityManager::Private::Private(const core::dbus::Bus::Ptr& bus)
+    : bus{bus},
+      bus_daemon{std::make_shared<core::dbus::DBus>(bus)}
 {
     try
     {
-        system_bus = std::make_shared<core::dbus::Bus>(core::dbus::WellKnownBus::system);
-        executor = dbus::asio::make_executor(system_bus);
-        system_bus->install_executor(executor);
-
-        worker = std::move(std::thread
-        {
-           [this]()
-           {
-               system_bus->run();
-           }
-        });
-
         setup_network_stack_access();
+    }
+    catch (const std::exception& e)
+    {
+        SYSLOG(INFO) << "Error while setting up access to the networking stack: " << e.what();
+        // An error occured while setting up access to the networking stack.
+        // For that, we setup watching of registration changes and initialize
+        // the instance later on.
+        network_manager_watcher = bus_daemon->make_service_watcher(
+                    xdg::NetworkManager::name(),
+                    core::dbus::DBus::WatchMode::registration);
+
+        network_manager_watcher->service_registered().connect([this]()
+        {
+            setup_network_stack_access();
+        });
+    }
+
+    try
+    {
         setup_radio_stack_access();
     }
     catch (const std::exception& e)
     {
-        SYSLOG(ERROR) << "Error while setting up access to radio and network stack: " << e.what();
+        SYSLOG(INFO) << "Error while setting up access to the radio stack: " << e.what();
+        // An error occured while setting up access to the radio stack.
+        // For that, we setup watching of registration changes and initialize
+        // the instance later on.
+        modem_manager_watcher = bus_daemon->make_service_watcher(
+                    org::Ofono::name(),
+                    core::dbus::DBus::WatchMode::registration);
+
+        modem_manager_watcher->service_registered().connect([this]()
+        {
+            setup_radio_stack_access();
+        });
     }
 }
 
-impl::OfonoNmConnectivityManager::Private::~Private()
+connectivity::OfonoNmConnectivityManager::Private::~Private()
 {
-    if (system_bus)
-        system_bus->stop();
-
-    if (worker.joinable())
-        worker.join();
-
     dispatcher.service.stop();
 
     if (dispatcher.worker.joinable())
         dispatcher.worker.join();
 }
 
-void impl::OfonoNmConnectivityManager::Private::setup_radio_stack_access()
+void connectivity::OfonoNmConnectivityManager::Private::setup_radio_stack_access()
 {
-    modem_manager.reset(new org::Ofono::Manager(system_bus));
+    modem_manager.reset(new org::Ofono::Manager(bus));
 
     modem_manager->for_each_modem([this](const core::dbus::types::ObjectPath& path)
     {
@@ -199,7 +219,7 @@ void impl::OfonoNmConnectivityManager::Private::setup_radio_stack_access()
     });
 }
 
-void impl::OfonoNmConnectivityManager::Private::on_modem_added(const core::dbus::types::ObjectPath& path)
+void connectivity::OfonoNmConnectivityManager::Private::on_modem_added(const core::dbus::types::ObjectPath& path)
 {
     auto modem = modem_manager->modem_for_path(path);
 
@@ -262,7 +282,7 @@ void impl::OfonoNmConnectivityManager::Private::on_modem_added(const core::dbus:
     signals.connected_cell_added(cell_result.first->second);
 }
 
-void impl::OfonoNmConnectivityManager::Private::on_modem_removed(const core::dbus::types::ObjectPath& path)
+void connectivity::OfonoNmConnectivityManager::Private::on_modem_removed(const core::dbus::types::ObjectPath& path)
 {
     detail::CachedRadioCell::Ptr cell;
     {
@@ -288,7 +308,7 @@ void impl::OfonoNmConnectivityManager::Private::on_modem_removed(const core::dbu
     if (cell) signals.connected_cell_removed(cell);
 }
 
-void impl::OfonoNmConnectivityManager::Private::on_modem_interfaces_changed(
+void connectivity::OfonoNmConnectivityManager::Private::on_modem_interfaces_changed(
         const core::dbus::types::ObjectPath& path,
         const std::vector<std::string>& interfaces)
 {
@@ -352,9 +372,9 @@ void impl::OfonoNmConnectivityManager::Private::on_modem_interfaces_changed(
     }
 }
 
-void impl::OfonoNmConnectivityManager::Private::setup_network_stack_access()
+void connectivity::OfonoNmConnectivityManager::Private::setup_network_stack_access()
 {
-    network_manager.reset(new xdg::NetworkManager(system_bus));
+    network_manager.reset(new xdg::NetworkManager(bus));
 
     network_manager->for_each_device([this](const core::dbus::types::ObjectPath& device_path)
     {
@@ -453,9 +473,35 @@ void impl::OfonoNmConnectivityManager::Private::setup_network_stack_access()
     {
         state.set(from_nm_property(s));
     });
+
+    // Read initial properties.
+    cached.is_wifi_enabled.set(network_manager->properties.is_wifi_enabled.get());
+    cached.is_wifi_hardware_enabled.set(network_manager->properties.is_wifi_hardware_enabled.get());
+    cached.is_wwan_enabled.set(network_manager->properties.is_wwan_enabled.get());
+    cached.is_wwan_hardware_enabled.set(network_manager->properties.is_wwan_hardware_enabled.get());
+    // And connect to changed signals.
+    network_manager->properties.is_wifi_enabled->changed().connect([this](bool value)
+    {
+        cached.is_wifi_enabled = value;
+    });
+
+    network_manager->properties.is_wifi_hardware_enabled->changed().connect([this](bool value)
+    {
+        cached.is_wifi_hardware_enabled = value;
+    });
+
+    network_manager->properties.is_wwan_enabled->changed().connect([this](bool value)
+    {
+        cached.is_wwan_enabled = value;
+    });
+
+    network_manager->properties.is_wwan_hardware_enabled->changed().connect([this](bool value)
+    {
+        cached.is_wwan_hardware_enabled = value;
+    });
 }
 
-void impl::OfonoNmConnectivityManager::Private::on_access_point_added(
+void connectivity::OfonoNmConnectivityManager::Private::on_access_point_added(
         const core::dbus::types::ObjectPath& ap_path,
         const core::dbus::types::ObjectPath& device_path)
 {
@@ -480,7 +526,7 @@ void impl::OfonoNmConnectivityManager::Private::on_access_point_added(
     ul.unlock(); signals.wireless_network_added(wifi);
 }
 
-void impl::OfonoNmConnectivityManager::Private::on_access_point_removed(const core::dbus::types::ObjectPath& ap_path)
+void connectivity::OfonoNmConnectivityManager::Private::on_access_point_removed(const core::dbus::types::ObjectPath& ap_path)
 {
     std::unique_lock<std::mutex> ul(cached.guard);
 
@@ -499,7 +545,7 @@ void impl::OfonoNmConnectivityManager::Private::on_access_point_removed(const co
     ul.unlock(); signals.wireless_network_removed(wifi);
 }
 
-connectivity::Characteristics impl::OfonoNmConnectivityManager::Private::characteristics_for_connection(const core::dbus::types::ObjectPath& path)
+connectivity::Characteristics connectivity::OfonoNmConnectivityManager::Private::characteristics_for_connection(const core::dbus::types::ObjectPath& path)
 {
     xdg::NetworkManager::ActiveConnection ac
     {
@@ -567,8 +613,50 @@ connectivity::Characteristics impl::OfonoNmConnectivityManager::Private::charact
     return characteristics;
 }
 
+namespace
+{
+struct Runtime
+{
+    static Runtime& instance()
+    {
+        static Runtime runtime;
+        return runtime;
+    }
+
+    Runtime()
+        : system_bus{std::make_shared<core::dbus::Bus>(core::dbus::WellKnownBus::system)},
+          executor{core::dbus::asio::make_executor(system_bus)}
+    {
+        system_bus->install_executor(executor);
+
+        worker_thread = std::move(std::thread
+        {
+            [this]() { system_bus->run(); }
+        });
+    }
+
+    ~Runtime()
+    {
+        system_bus->stop();
+
+        if (worker_thread.joinable())
+            worker_thread.join();
+    }
+
+    core::dbus::Bus::Ptr system_bus;
+    core::dbus::Executor::Ptr executor;
+    std::thread worker_thread;
+};
+}
 const std::shared_ptr<connectivity::Manager>& connectivity::platform_default_manager()
 {
-    static const std::shared_ptr<connectivity::Manager> instance{new impl::OfonoNmConnectivityManager{}};
+    static const std::shared_ptr<connectivity::Manager> instance
+    {
+        new connectivity::OfonoNmConnectivityManager
+        {
+            Runtime::instance().system_bus
+        }
+    };
+
     return instance;
 }
