a8caaa8b5802555fbdee30b6093230d646e14be3
Robert Bruce Park <robert.park@canonical.com>
2014-10-27 14:58:16 -0700

    Releasing 2.1+14.10.20141023-0ubuntu1.

--- include/location_service/com/ubuntu/location/provider_factory.h
+++ include/location_service/com/ubuntu/location/provider_factory.h
@@ -43,8 +43,15 @@ class ProviderFactory
 
     static ProviderFactory& instance();
 
+    // Makes the given factory functor known for the given name.
     void add_factory_for_name(const std::string& name, const Factory& factory);
 
+    // Tries to lookup the factory for the given name, and create a provider instance
+    // by calling the factory and passing it the given config. Please note that the name
+    // can be decorated with @ for distinguishing providers of the same type but different configs.
+    // Eg.:
+    // --provider=remote::Provider@espoo --remote::Provider@espoo::name="com.ubuntu.espoo.service.Provider" --remote::Provider@espoo::path="/com/ubuntu/espoo/service/Provider"
+    // --provider=remote::Provider@gps --remote::Provider@gps::name="com.ubuntu.android.gps.Provider" --remote::Provider@gps::path="/com/ubuntu/android/gps/Provider"
     Provider::Ptr create_provider_for_name_with_config(const std::string& name, const Configuration& config);
 
     void enumerate(const std::function<void(const std::string&, const Factory&)>& enumerator);
