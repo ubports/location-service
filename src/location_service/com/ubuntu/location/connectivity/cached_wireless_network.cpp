087f7db44c5e814c5ec7070f50127e7d574f7f8f
Thomas Voß <thomas.voss@canonical.com>
2015-11-12 22:50:20 +0100

    Log destruction of CachedWirelessNetwork instances.

--- src/location_service/com/ubuntu/location/connectivity/cached_wireless_network.cpp
+++ src/location_service/com/ubuntu/location/connectivity/cached_wireless_network.cpp
@@ -115,6 +115,7 @@ detail::CachedWirelessNetwork::CachedWirelessNetwork(
 
 detail::CachedWirelessNetwork::~CachedWirelessNetwork()
 {
+    LOG(INFO) << __PRETTY_FUNCTION__;
     access_point_.properties_changed->disconnect(connections.ap_properties_changed);
 }
 
