ab0548374bb2524e5a3bfa54de582a34d57b1394
Scott Sweeny <scott.sweeny@canonical.com>
2015-12-01 09:48:00 -0500

    Add myself as an author of the test that I stole/wrote

--- tests/delayed_service_test.cpp
+++ tests/delayed_service_test.cpp
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ *              Scott Sweeny <scott.sweeny@canonical.com>
  */
 
 #include <com/ubuntu/location/service/daemon.h>
