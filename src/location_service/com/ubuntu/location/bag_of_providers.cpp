edf35111406c1b98038418a00d9360d8091cc6e7
Scott Sweeny <scott.sweeny@canonical.com>
2016-02-08 14:56:39 -0500

    Factor BagOfProviders out into its own class so it can be reused

---
+++ src/location_service/com/ubuntu/location/bag_of_providers.cpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Scott Sweeny <scott.sweeny@canonical.com
+ */
+#include "bag_of_providers.h"
+
+namespace cul = com::ubuntu::location;
+
+
+cul::Provider::Features all_features()
+{
+    return cul::Provider::Features::position |
+           cul::Provider::Features::heading |
+           cul::Provider::Features::velocity;
+}
+
+cul::Provider::Requirements all_requirements()
+{
+    return cul::Provider::Requirements::cell_network |
+           cul::Provider::Requirements::data_network |
+           cul::Provider::Requirements::monetary_spending |
+           cul::Provider::Requirements::satellites;
+}
+
+cul::BagOfProviders::BagOfProviders(const std::set<cul::Provider::Ptr>& providers)
+    : Provider{all_features(), all_requirements()},
+      providers{providers}
+{
+    // We connect to all updates of each provider.
+    for (auto provider : BagOfProviders::providers)
+    {
+        connections.push_back(provider->updates().position.connect([this](const cul::Update<cul::Position>& update)
+        {
+            mutable_updates().position(update);
+        }));
+
+        connections.push_back(provider->updates().heading.connect([this](const cul::Update<cul::Heading>& update)
+        {
+            mutable_updates().heading(update);
+        }));
+
+        connections.push_back(provider->updates().velocity.connect([this](const cul::Update<cul::Velocity>& update)
+        {
+            mutable_updates().velocity(update);
+        }));
+    }
+
+}
+
+// We always match :)
+bool cul::BagOfProviders::matches_criteria(const cul::Criteria&)
+{
+    return true;
+}
+
+// We forward all events to the other providers.
+void cul::BagOfProviders::on_wifi_and_cell_reporting_state_changed(cul::WifiAndCellIdReportingState state)
+{
+    for (auto provider : providers)
+        provider->on_wifi_and_cell_reporting_state_changed(state);
+}
+
+void cul::BagOfProviders::on_reference_location_updated(const cul::Update<cul::Position>& position)
+{
+    for (auto provider : providers)
+        provider->on_reference_location_updated(position);
+}
+
+void cul::BagOfProviders::on_reference_velocity_updated(const cul::Update<cul::Velocity>& velocity)
+{
+    for (auto provider : providers)
+        provider->on_reference_velocity_updated(velocity);
+}
+
+void cul::BagOfProviders::on_reference_heading_updated(const cul::Update<cul::Heading>& heading)
+{
+    for (auto provider : providers)
+        provider->on_reference_heading_updated(heading);
+}
+
+void cul::BagOfProviders::start_position_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->start_position_updates();
+}
+
+void cul::BagOfProviders::stop_position_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->stop_position_updates();
+}
+
+void cul::BagOfProviders::start_heading_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->start_heading_updates();
+}
+
+void cul::BagOfProviders::stop_heading_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->stop_heading_updates();
+}
+
+void cul::BagOfProviders::start_velocity_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->start_velocity_updates();
+}
+
+void cul::BagOfProviders::stop_velocity_updates()
+{
+    for (auto provider : providers)
+        provider->state_controller()->stop_velocity_updates();
+}
